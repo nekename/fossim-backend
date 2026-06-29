4f1100fae6238ddcb5df1e3813da0dbe49cb666f
nekename <63245705+nekename@users.noreply.github.com>
2026-06-29 15:37:52 +0100

    feat: add rocket_cors to include relevant access control headers in responses

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,14 @@ mod api;
 #[launch]
 fn rocket() -> _ {
 	dotenvy::dotenv().expect(".env file should be present");
-	rocket::build().mount("/", routes![api::oauth::github::client_id])
+
+	let cors = rocket_cors::CorsOptions {
+		..Default::default()
+	}
+	.to_cors()
+	.expect("CORS options should be valid");
+
+	rocket::build()
+		.mount("/", routes![api::oauth::github::client_id])
+		.attach(cors)
 }
